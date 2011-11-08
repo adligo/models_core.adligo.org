b3832981de178a6651a0a0cecbc04bc4c7a76f7c
scott <scott@adligo.com>
2011-11-08 21:36:27 +0000

    added setType

--- src/org/adligo/models/core/client/I_OrganizationMutant.java
+++ src/org/adligo/models/core/client/I_OrganizationMutant.java
@@ -1,4 +1,5 @@
 package org.adligo.models.core.client;
 
 public interface I_OrganizationMutant extends I_Organization, I_NamedIdMutant, I_StorageMutant {
+	public void setType(I_NamedId p) throws InvalidParameterException;
 }
