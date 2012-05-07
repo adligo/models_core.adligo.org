35eccf4397df32b99fe2871c4ca1176ef45220a1
scott <scott@adligo.com>
2012-05-07 21:34:34 +0000

    fixed missing period

--- src/org/adligo/models/core/client/ModelsCoreEnglishConstants.java
+++ src/org/adligo/models/core/client/ModelsCoreEnglishConstants.java
@@ -2,7 +2,7 @@ package org.adligo.models.core.client;
 
 
 public class ModelsCoreEnglishConstants implements I_ModelsCoreConstants {
-	public static final String STORAGE_IDENTIFIER_REQUIRED = "A Storage Identifier is required";
+	public static final String STORAGE_IDENTIFIER_REQUIRED = "A Storage Identifier is required.";
 	
 	public static final String EMAIL_ATTACHMENT_MAY_NOT_BE_EMPTY = "A E-mail attachment may NOT be empty.";
 	public static final String EMAIL_ATTACHEMNT_MAY_NOT_BE_NULL = "A E-mail attachemnt may NOT be null.";
