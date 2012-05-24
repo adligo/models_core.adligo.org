c9a2a71a8b24a56ffa04f0da35abeec83f417317
scott <scott@adligo.com>
2012-05-24 21:58:15 +0000

    some code cleaning

--- src/org/adligo/models/core/client/ModelsCoreRegistry.java
+++ src/org/adligo/models/core/client/ModelsCoreRegistry.java
@@ -1,11 +1,14 @@
 package org.adligo.models.core.client;
 
+import org.adligo.i.adi.client.I18nConstantsFactory;
+
 
 public class ModelsCoreRegistry {
 
 	public static void setup() {
 		//setup english constants for servers unless overridden
-		new ModelsCoreEnglishConstants();
+		I18nConstantsFactory.INSTANCE.put(I_ModelsCoreConstants.class, 
+				new ModelsCoreEnglishConstants());
 		
 	}
 }
