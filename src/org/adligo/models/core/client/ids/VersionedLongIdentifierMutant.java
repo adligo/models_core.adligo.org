2b86add1c78cbc2ca42186863f28f5f86ef7c29a
scott <scott@adligo.com>
2012-05-24 06:43:56 +0000

    other hibernate id stuff

--- src/org/adligo/models/core/client/ids/VersionedLongIdentifierMutant.java
+++ src/org/adligo/models/core/client/ids/VersionedLongIdentifierMutant.java
@@ -45,4 +45,38 @@ public class VersionedLongIdentifierMutant implements I_VersionedLongIdentifier 
 		this.version = version;
 	}
 	
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((version == null) ? 0 : version.hashCode());
+		return result;
+	}
+	
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		VersionedLongIdentifierMutant other = (VersionedLongIdentifierMutant) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (version == null) {
+			if (other.version != null)
+				return false;
+		} else if (!version.equals(other.version))
+			return false;
+		return true;
+	}
+	
+	public String toString() {
+		return "VersionedLongIdentifierMutant [version=" + version + ", id="
+				+ id + "]";
+	}
+	
 }
