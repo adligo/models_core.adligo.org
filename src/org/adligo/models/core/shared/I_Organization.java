cc902e7acd39fd964c9f4b2b9cfc4434210c7b32
scott <scott@adligo.com>
2014-03-05 02:04:35 -0600

    pruning out some old methods apis

--- src/org/adligo/models/core/shared/I_Organization.java
+++ src/org/adligo/models/core/shared/I_Organization.java
@@ -3,6 +3,4 @@ package org.adligo.models.core.shared;
 
 public interface I_Organization extends I_NamedId, I_Customizable, I_Changeable {
 	public I_StorageIdentifier getType();
-	public I_Organization toImmutable() throws ValidationException;
-	public I_OrganizationMutant toMutant() throws ValidationException;
 }
